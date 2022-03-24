bfc84262f4af6ce47a3cdfe6db99d80e8da9515d
Hjeongwoo <87626378+Hjeongwoo@users.noreply.github.com>
2022-03-24 22:40:43 +0900

    2022.03.24

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Componens/Header";
+import "./App.css";
 
 function App() {
   return <Header />;
